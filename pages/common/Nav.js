baadfc93f2ee152cedb481bb1409ec9da786c3eb
parkjungkwan <pakjkwan@gmail.com>
2022-04-07 17:21:04 +0900

    17

--- pages/common/Nav.js
+++ pages/common/Nav.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "./style/Nav.module.css";
+import styles from "./styles/Nav.module.css";
 
 export default function Nav(){
   return (
@@ -15,9 +15,9 @@ export default function Nav(){
         <li className={styles.li}> <Link href='/game/game-list'>GameList</Link> </li>
         <li className={styles.li}> <Link href='/game/team-list'>TeamList</Link> </li>
         <li className={styles.li}> <Link href='/todo/todo-list'>TodoList</Link> </li>
-        <li className={styles.li}> <Link href='/user/sign-in'>SignIn</Link> </li>
-        <li className={styles.li}> <Link href='/user/sign-up'>SignUp</Link> </li>
-        
+        <li className={styles.li}> <Link href='/user/sign-in'>회원가입</Link> </li>
+        <li className={styles.li}> <Link href='/user/sign-up'>로그인</Link> </li>
+        <li className={styles.li}> <Link href='/user/user-list'>사용자목록</Link> </li>
         
       </ul>
     </nav>
