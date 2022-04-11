02f7b26b634eab7e91818042c27b048176a9b487
parkjungkwan <pakjkwan@gmail.com>
2022-04-11 16:40:31 +0900

    16:40

--- redux/reducers/admin.reducer.js
+++ redux/reducers/admin.reducer.js
@@ -1,10 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit"
+import { HYDRATE } from "next-redux-wrapper"
 
 const initialState = {}
 
 export const adminSlice = createSlice({
-    name: '',
+    name: 'admin',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: {
+        [HYDRATE] : () => {
+            console.log(action.payload)
+            return action.payload.admin
+        }
+    }
 })
 export default adminSlice.reducer
